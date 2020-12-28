09da1b1d31d09ecb4e43d7b8aed8be366bd23ede
mcwojdzinski <mateuszcwojdzinski@outlook.com>
2020-12-28 19:36:42 +0100

    [COV-19] Add InfoBox component for the site, with fixing bug with taking a data from disease.sh api

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,56 @@
 <template>
-  <Header
-    @handleCountryChange="handleCountryChange"
-  />
-  <h1>{{ setCountry }}</h1>
+  <div class="wrapper">
+    <div class="left__content">
+      <Header
+      @handleCountryChange="handleCountryChange"
+      />
+      <div class="card__wrapper">
+        <InfoBox 
+        title="Coronavirus Cases"
+        cases=123123
+        total=20000
+        />
+        <InfoBox 
+        title="Recovered"
+        cases=123123
+        total=20000
+        />
+        <InfoBox
+        title="Deaths"
+        cases=123123
+        total=20000
+        />
+      </div>
+      <Map />
+      <h1>{{ setCountry }}</h1>
+    </div>
+    <div class="right__content">
+      <Table />
+      <Graph />
+    </div>
+  </div>
 </template>
 
 <script>
-import  Header  from './components/header/header.vue'
+import  Header  from './components/Header/Header.vue'
+import InfoBox from './components/InfoBox/InfoBox.vue'
+import Map from './components/Map/Map.vue'
+import Table from './components/Table/Table.vue'
+import Graph from './components/Graph/Graph.vue'
 
 export default {
   name: 'App',
   components:{
-    Header
+    Header,
+    InfoBox,
+    Map,
+    Table,
+    Graph
   },
   data: function(){
       return {
           setCountry: 'all',
+          TotalCases:'Total Cases'
       }
   },
   methods:{
@@ -32,5 +67,35 @@ export default {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  .wrapper{
+    display: flex;
+    flex-direction: column;
+
+    @media (min-width: 1440px){
+      flex-direction: row;
+    }
+
+    .left__content{
+      width: 100vw;
+
+      @media (min-width: 1440px){
+        width: 70vw;
+      }
+    }
+
+    .right__content{
+      width: 100vw;
+
+      @media (min-width: 1440px){
+        width: 30vw;
+      }
+    }
+  }
+
+  .card__wrapper{
+    display: flex;
+    flex-direction: row;
   }
 </style>
---
+++ src/components/InfoBox/InfoBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="card">
+        <h1>{{ title }}</h1>
+        <p>{{cases}}</p>
+        <p>{{total}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: "InfoBox",
+    props: {
+        title: String,
+        total: Number,
+        cases: Number
+    }
+}
+</script>
+<style lang="scss">
+    .card{
+        width: 33.333%;
+        height: 20vh;
+        background-color: lightblue;
+        margin: 5px;
+        padding: 10px;
+        border-radius: 10px;
+    }
+</style>
