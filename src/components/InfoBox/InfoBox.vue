69082b28624276df9a2e44971c8263e3c821d3a7
mcwojdzinski <mateuszcwojdzinski@outlook.com>
2021-01-30 10:53:20 +0100

    [COV-19] Change component styling for better view

--- src/components/InfoBox/InfoBox.vue
+++ src/components/InfoBox/InfoBox.vue
@@ -19,7 +19,9 @@ export default {
     .card{
         width: 33.333%;
         height: 20vh;
-        background-color: lightblue;
+        background-color: white;
+        padding: 1rem;
+        box-shadow: 0 0 8px -4px rgba(0,0,0,0.5);
         margin: 5px;
         padding: 10px;
         border-radius: 10px;
